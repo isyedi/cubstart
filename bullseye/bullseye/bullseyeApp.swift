363e5774511dd14cd62bc8170383717e9e013e8a
Irfan Syed <irfan.syed@live.com>
2022-02-19 20:06:28 -0800

    final submission

---
+++ bullseye/bullseye/bullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeApp.swift
+//  bullseye
+//
+//  Created by Irfan Syed on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct bullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
